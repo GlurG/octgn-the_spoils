965fdfcb1e4a10988c1e39a0d43fad8aa9d3ac9f
GlurG <martin.tberger@gmail.com>
2016-06-07 21:58:19 +0200

    Changelog reset :)

--- the_spoils/Scripts/changelog.py
+++ the_spoils/Scripts/changelog.py
@@ -1,25 +1,5 @@
 changelog = {
-	"1.4.2.0": [
-		"Added this changelog dialog to inform about changes in versions.",
-		"Added autoplacments of cards (with support for more than 2 players). You can customize the layout by \"Table Menu -> Game Settings -> Layout\".",
-		"Added a response system (with support for more than 2 players). The game host deceides seating order and who is a spectator. It can later be changed by \"Table Menu -> Game Settings -> Seating order\".",
-		"Added Basic Rules document to Game Documents.",
-		"Added OCTGN Interface document to Game Documents. Read it, alot of shortcuts have changed.",
-		"Moving cards from your piles to the table does not trigger a respond. Please read the OCTGN Interface document.",
-	],
-	"1.4.3.0": [
-		"Cards you don't own are moved to it's owners Discard Pile or Out of Game on the individual functions. Move to Hand or Draw Deck print a message if you don't own the card. With this change you no longer need to pass control of a card you don't own and ask your opponent to destroy it."
-	],
-	"1.4.3.4": [
-		"F3-5 ask for confirmation when it's not your turn",
-		"\"use ability\" and \"flip up\" only work on single cards",
-		"\"flip down\" automatically restores the cards when it was depleted"
-	],
-	"1.4.4.0": [
-		"Added location damage markers",
-		"Added \"Don't Restore\" card function",
-	],
-	"1.4.4.4": [
-		"Added \"Alt + M\" shortcut to create a single Micromajig"
-	],
+	"1.0.0.0": [
+		"Initial release! :)"
+	]
 }
