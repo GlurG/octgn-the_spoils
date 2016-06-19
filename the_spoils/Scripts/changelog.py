2a7cf7e2717da58ad87ba7b964ff8309646b0fa4
GlurG <martin.tberger@gmail.com>
2016-06-19 18:00:06 +0200

    Bump version to 2.0.0.0 Include a warning text about deck conversion

--- the_spoils/Scripts/changelog.py
+++ the_spoils/Scripts/changelog.py
@@ -1,6 +1,6 @@
 changelog = {
-	"1.4.5.0": [
-		"Ungodly Mess added.",
-		"Packaging information re-added to each set to enable Limited Games."
+	"2.0.0.0": [
+		"IMPORTANT: Your Spoils decks created with the previous version will have to be converted in order to work. To convert them, visit https://www.spoilstoolbox.com/deck-builder. Furthermore, any image packs you have installed will have to be downloaded again from https://www.spoilstoolbox.com.",
+		"Packaging information added to each set to enable Limited Games.",
 	]
 }
